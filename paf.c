dc538430fa9c058a9ceee5b796d61bcf39f8e081
Bastian Escalona Morales <bastianescalona@hotmail.com>
2015-07-04 23:03:46 -0400

    paf
    error con espacios y intento de trabajo en comentarios borrados

--- paf.c
+++ paf.c
@@ -6,7 +6,7 @@
 #define N 200
 int main(){
 
-	FILE *lee;                                      	      //Declaracion de variables.
+	FILE *lee; //Declaracion de variables.
 	FILE *resultado;
 	int i=0,j=0,k=1;
     int cont=0;
@@ -15,47 +15,47 @@ int main(){
 	int *pu;
     int *yoshi;
 	char array[N];
-    for (i = 0; i < N; ++i) array[i]=0;                       //Se recorre el arreglo.
+    for (i = 0; i < N; ++i) array[i]=0; //Se recorre el arreglo.
 	int minimadif=0;
 	int suma1=0,suma2=0,suma3=0,suma4=0;
 	int ext[N];
-	for (i = 0; i < N; ++i) ext[i]=0; 					      //Se recorre el arreglo.
+	for (i = 0; i < N; ++i) ext[i]=0; //Se recorre el arreglo.
 	int array2[N];
-	for (i = 0; i < N; ++i) array2[i]=0; 				      //Se recorre el arreglo.
-	lee=fopen("entrada.ent","r");						      //Se abre el archivo donde estan nuestros numeros.
+	for (i = 0; i < N; ++i) array2[i]=0; //Se recorre el arreglo.
+	lee=fopen("entrada.ent","r"); //Se abre el archivo donde estan nuestros numeros.
 	if (lee == NULL)
 		printf("Error al leer el archivo\n");
 	else{
-        fseek(lee,hola,0);									  //Lo usamos para "saltar" e ignorar la cantidad de numeros y el ENTER del archivo.
+        fseek(lee,hola,0); //Lo usamos para "saltar" e ignorar la cantidad de numeros y el ENTER del archivo.
 		fgets(array,N,lee);
-        if(array[0] < 48 || array[0]>58){                     //Abrimos el archivo y analizamos.
+        if(array[0] < 48 || array[0]>58){ //Abrimos el archivo y analizamos.
             hola++;
         }
         fseek(lee,hola,0);
-        fgets(array,N,lee);                                   //Abrimos el archivo y analizamos.
+        fgets(array,N,lee); //Abrimos el archivo y analizamos.
         if(array[0] < 48 || array[0] >58){
             hola++;
         }
-        fseek(lee,hola,0);                                    //Abrimos el archivo en la posicion donde esten nuestros numeros.
+        fseek(lee,hola,0); //Abrimos el archivo en la posicion donde esten nuestros numeros.
         fgets(array,N,lee);
 	}
 	p=&array[0];
-	for(i=0; i<N;i++){									      //Pasamos los caracteres del archivo (ascii) a enteros.
-            if(48 < *p && *p < 58){
-            	array2[j]= *p -48;						      //Los guardamos en un arreglo entero.
-            	printf("%d",array2[j]);
+	for(i=0; i<N;i++){
+            if(48 < *p && *p < 58){ //Pasamos los caracteres del archivo (ascii) a enteros.
+            	array2[j]= *p -48;
+            	printf("%d",array2[j]); //Guardamos los numeros enteros en un arreglo. 
             	j++;
             }
             *p++;
     }
-   	printf("\n");                                             //Usamos un contador para evitar iteracciones vacias.
+   	printf("\n"); //Usamos un contador para evitar iteracciones vacias.
     for (i = 0; i < N; ++i){
         if (array2[i]!=0) cont++;
         else break;
     }
     printf("La cantidad de numeros son :  %d\n",cont );
     pu=&array2[0];
-    for (i = 0; i < cont; ++i){			                      //Hacemos una copia del arreglo entero (Para evitar problemas en las operaciones)
+    for (i = 0; i < cont; ++i){ //Hacemos una copia del arreglo entero (Para evitar problemas en las operaciones)
     	ext[i]= *pu;
     	*pu++;
         if (*pu==0) break;
@@ -63,22 +63,22 @@ int main(){
     yoshi=&ext[0];
     pu=&array2[0];
     while(k<cont){
-        for(i=0;i<k;i++){                                     //Sumamos de la posicion 0 hasta que el valor de "i" no sobrepase a "k" (Mientras "k" cambia la posicion de la suma igual lo hara)
+        for(i=0;i<k;i++){ //Sumamos de la posicion 0 hasta que el valor de "i" no sobrepase a "k" (Mientras "k" cambia la posicion de la suma igual lo hara)
             suma1=*(pu+i)+suma1;
         }
-        for (j = i; j < cont; ++j){                           //Se comenzara a sumar desde la ultima posicion que tuvo acceso "i".
+        for (j = i; j < cont; ++j){ //Se comenzara a sumar desde la ultima posicion que tuvo acceso "i".
             suma2=*(yoshi+j)+suma2;
         }
-        if(k==1){                                             //Este es el primer caso cuando a la primera posicion se le resta a las demas.
+        if(k==1){ //Este es el primer caso cuando a la primera posicion se le resta a las demas.
             suma3=(suma1-suma2);
             if (suma3 < 0){
-                suma3 = suma3 * -1;                           //Si el numero es negativo se multiplicara por -1 para que quede positivo.
+                suma3 = suma3 * -1; //Si el numero es negativo se multiplicara por -1 para que quede positivo.
             }
         }
-        if(k>1){                                              //Este es el caso cuando se le suma a la primera posicion la posicion que continua y se le resta con las sobrantes.
+        if(k>1){ //Este es el caso cuando se le suma a la primera posicion la posicion que continua y se le resta con las sobrantes.
             suma4=(suma1-suma2);
             if (suma4 < 0){
-                suma4 = suma4 * -1;                           //Si el numero es negativo se multiplicara por -1 para que quede positivo.
+                suma4 = suma4 * -1; //Si el numero es negativo se multiplicara por -1 para que quede positivo.
             }
             if(suma4 < suma3){
                 suma3=suma4;
@@ -89,134 +89,8 @@ int main(){
         k++;
     }
     minimadif = suma3;
-    printf("El minimo divisor es : %d\n",minimadif);          //Se muestra en pantalla el minimo divisor.
-    resultado=fopen("salida.sal","w+b");                      //Crea el archivo "salida.sal".
-    fprintf(resultado, "%d", minimadif);                      //Ingresa el valor de la minima diferencia al archivo creado.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    pu=&array2[0];
-    puntero = &ext[1];
-    for (k = 1; k< N; ++k){								      //Comenzamos en la primera operacion.
-        suma1 = *puntero + suma1;
-    	*puntero++;
-    	if (k==4){
-    		suma1 = abs(*pu - suma1);					      //Restamos las sumas y obtenemos la diferencia absoluta.
-    	}
-    }
-    pu=&array2[0];										      //Posicion de punteros "reiniciada" a comodidad.
-    puntero=&ext[2];
-    for (k = 2; k< 5; ++k){								      //Hacemos la segunda operacion.
-        suma2 = *puntero + suma2;
-    	*puntero++;
-    	if (k==4){
-    		for (i = 0; i < 2; ++i){
-   				suma5 = suma5 + *pu;
-   				*pu++;
-    		}
-   			suma2 = abs(suma5 - suma2);					      //Restamos las sumas y obtenemos la diferencia absoluta.
-    	}
-    }
-    pu=&array2[0];										      //Posicion de punteros "reiniciada" a comodidad.
-    puntero=&ext[3];
-    suma5=0;
-    for (k = 3; k < 5; ++k){							      //Hacemos la tercera operacion.
-    	suma3 = *puntero + suma3;
-    	*puntero++;
-    	if (k==4){
-    		for (i = 0; i < 3; ++i){
-    			suma5 = suma5 + *pu;
-    			*pu++;
-    		}
-    		suma3 = abs(suma5 - suma3);					      //Restamos las sumas y obtenemos la diferencia absoluta.
-    	}
-    }
-    pu=&array2[0];
-    puntero=&ext[4];									       //Posicion de punteros "reiniciada" a comodidad.
-    suma5=0;
-    for (k = 4; k < 5; ++k){
-    	suma4 = *puntero + suma4;
-    	if (k==4){
-    		for (i = 0; i < 4; ++i){
-    			suma5 = suma5 + *pu;
-    			*pu++;
-    		}
-    		suma4= abs(suma5 - suma4);                          //Restamos las sumas y obtenemos la diferencia absoluta.
-    	}
-	}
-	if (suma1 < suma2 && suma1 < suma3 && suma1 < suma4){	    //Buscamos cual es la diferencia absoluta menor.
-		minimadif = suma1;									    //Y igualamos la minima diferencia a la menor ya sea el caso.
-	}
-	if (suma2 < suma1 && suma2 <= suma3 && suma2 < suma4){
-		minimadif = suma2;
-	}
-	if (suma3 < suma1 && suma3 < suma2 && suma3 < suma4){
-		minimadif = suma3;
-	}
-	if (suma4 < suma1 && suma4 < suma2 && suma4 < suma3){
-		minimadif = suma4;
-	}
-	*/
+    printf("El minimo divisor es : %d\n",minimadif); //Se muestra en pantalla el minimo divisor.
+    resultado=fopen("salida.sal","w+b"); //Crea el archivo "salida.sal".
+    fprintf(resultado, "%d", minimadif); //Ingresa el valor de la minima diferencia al archivo creado.
     return 0;
 }
